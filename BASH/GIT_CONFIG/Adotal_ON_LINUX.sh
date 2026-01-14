64a9630725c2c215e90d2b6db307abb28221693f
Adotal <adotal1484@gmail.com>
2026-01-14 17:42:03 -0600

    GIT config BASH files added

---
+++ BASH/GIT_CONFIG/Adotal_ON_LINUX.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.name "Adotal"
+git config --global user.email adotal1484@gmail.com
+git config --global core.editor vim   
+git config --global init.defaultBranch main
+# "code --wait"
+git config --global core.autocrlf input
+
+echo "Configuraciones establecidas para Adotal en linux"
+echo "Para visualizar configuarión, ejecute: git config --global -e"
