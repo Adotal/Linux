2eac36f77f69f79ace93ec5bfc4f759ab58306ca
Adotal <adotal1484@gmail.com>
2026-01-29 15:21:49 -0600

    Systemd-boot uploaded

--- ARCHLINUX/dwl/NewWaylandDWL.sh
+++ ARCHLINUX/dwl/NewWaylandDWL.sh
@@ -11,7 +11,7 @@ cd
 # echo "exec dwl 2> ~/.dwl.log" >> /home/${USER}/.bash_profile
 
 # Install packages
-sudo pacman -Syu fastfetch htop tree libinput wayland wlroots0.19 wayland-protocols pkg-config brightnessctl wget foot firefox chromium seatd code gimp pcmanfm wmenu ttf-dejavu-nerd go acpi unzip zip python3 jdk11-openjdk grim slurp swayimg man-db lib32-vulkan-radeon android-tools libreoffice-still pipewire pipewire-pulse wireplumber udiskie kdenlive lm_sensors mpv zathura zathura-pdf-mupdf imagemagick apache yt-dlp mariadb \
+sudo pacman -Syu fastfetch htop tree libinput wayland wlroots0.19 wayland-protocols pkg-config brightnessctl wget foot firefox chromium seatd code gimp pcmanfm wmenu ttf-dejavu-nerd go acpi unzip zip python3 jdk11-openjdk grim slurp swayimg man-db lib32-vulkan-radeon android-tools libreoffice-still pipewire pipewire-pulse wireplumber udiskie kdenlive lm_sensors mpv zathura zathura-pdf-mupdf imagemagick apache yt-dlp mariadb kicad kicad-library \
 libusb libffi openssl libgcrypt glib2 pixman sdl2 libslirp ccache dfu-util cmake python-pip # ESP-IDF DEPENDENCIES
 
 # dwl
@@ -32,3 +32,5 @@ libusb libffi openssl libgcrypt glib2 pixman sdl2 libslirp ccache dfu-util cmake
 # Enable: $ systemctl enable --user pipewire
 # Current mic vol: $ wpctl get-volume @DEFAULT_SOURCE@
 # Adjust mic vol: $ wpctl set-volume @DEFAULT_SOURCE@ .40
+#
+# # NOTE: VSCODE C/C++ EXTENSION NOT AVAILABLE IN OSS UNITL VERSION 1.20.5
